35e71bf317711eeb4dc5883fc7c08fd46f60e958
krishnadaskundu <krishnadas.kundu.unique@gmail.com>
2023-04-30 19:46:31 +0530

    practice

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ http.createServer((req,resp) =>
 resp.write("<h1>Code Step by step</h1>");
 resp.end();
 }).listen(4500);
+
+
+//used of http, function as parameter , arrow funcion, making a basic server
+
+
+/*
+const http=require('http');
+
+http.createServer((req, resp)=>{
+
+    resp.write("this is my first server");
+    resp.end();
+}).listen(4500);
+
+*/
