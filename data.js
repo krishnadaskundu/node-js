8a6f96bf0fa9362952cfca04210baf85ee480529
krishnadaskundu <krishnadas.kundu.unique@gmail.com>
2023-04-30 22:27:30 +0530

    practice

--- data.js
+++ data.js
@@ -4,3 +4,16 @@ const data=[
     {name:'peter',mail:'peter@test.com'}
 ]
 module.exports=data;
+
+
+/* 
+
+const data = [
+    {name:'anil',mail:'anil@test.com'},
+    {name:'sam',mail:'peter@test.com'},
+    {name:'peter',mail:'peter@test.com'}
+]
+
+module.exports= data;
+
+*/
--- index.js
+++ index.js
@@ -5,3 +5,18 @@ resp.writeHead(500,{'Content-Type':'application\json'});
 resp.write(JSON.stringify(data));
 resp.end();
 }).listen(5000);
+
+
+/* 
+
+  const http=require('http');
+  const data = require(./data');
+
+  http.createServer((req,resp) =>{
+      resp.writeHead(200, {'Content-Type': 'application\json'});
+      resp.write(JSON.stringify(data));
+      resp.end();
+
+  }).listen(5000);
+
+*/
