6b323deb092c0bec626d22b90e395e1165e035f3
krishnadaskundu <krishnadas.kundu.unique@gmail.com>
2023-04-30 19:05:46 +0530

    core module practice

---
+++ another.js
@@ -0,0 +1,3 @@
+const fs=require('fs').writeFileSync;
+fs("def.txt", "def");
+console.log("test");
--- index.js
+++ index.js
@@ -5,3 +5,20 @@ const result =arr.filter((item)=>{
     return item>=4
 });
 console.warn(result);
+
+/* 
+    core modules:
+    console (global module),
+    __filename (global module),
+    fs, 
+    Buffer used for processing data, 
+    HTTP used for API calls
+*/ 
+const fs = require('fs');
+// create the file named abc.text and 2nd parameter is contain in the text. 
+fs.writeFileSync('abc.txt', "This is fs 12");
+
+ //To console the directory name
+console.log(__dirname);
+//To console the file name
+console.log(__filename);
