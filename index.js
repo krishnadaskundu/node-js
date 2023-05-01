e8efd228a4d1239d13d50b45f37ed0507db431d9
krishnadaskundu <krishnadas.kundu.unique@gmail.com>
2023-05-01 12:56:54 +0530

    practice

--- index.js
+++ index.js
@@ -17,3 +17,18 @@ else
 
 
 // node index.js other red.txt 'this is color'
+/*
+const fs = require('fs');
+const input2 = process.argv;
+
+if (input2[2] == 'add') {
+    
+    fs.writeFileSync(input2[3],input2[4]);
+}else if (input2[2] == 'remove') {
+    fs.unlinkSync(input2[3])
+
+} else {
+    console.log("invalid input ")
+}
+
+*/
