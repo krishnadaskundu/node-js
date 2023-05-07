60350fdd52ae2f8e34b9f52878ee8e7fa19dd90d
krishnadaskundu <krishnadas.kundu.unique@gmail.com>
2023-05-07 20:21:54 +0530

    practice

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const app = express();
 
@@ -30,4 +31,46 @@ app.get("/help", (req, resp) => {
 });
 
 
-app.listen(5000);
+app.listen(5000);*/
+
+const express = require("express");
+const { get } = require("express/lib/response");
+
+const app = express();
+
+app.get('' ,(req, resp) =>{
+    
+    resp.send(`
+        <h1>welcome, ${req.query.name} </h1>
+        <a href="/about">about</a>
+        <a href="/help">help</a>
+        </br>
+        <input type="text" placeholder="name" value="${req.query.name}">
+    `)
+})
+
+app.get('/about' ,(req, resp) =>{
+    
+    resp.send(`
+    <h1>about us page</h1>
+    <a href="/">home</a>
+    <a href="/help">help</a>
+    `)
+})
+
+app.get('/help' ,(req, resp) =>{
+    
+    resp.send(`
+        <h1>help page</h1>
+        <a href="/">home</a>
+        <a href="/about">about</a>
+
+        [{name: 'krishna',
+        age: 26},
+        {name: 'rama',
+        age: 26}]
+
+    `)
+})
+
+app.listen(4848);
