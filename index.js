c4b42c9adb94e25783e757a1c9de43051c4a65e0
krishnadaskundu <krishnadas.kundu.unique@gmail.com>
2023-05-07 20:43:52 +0530

    practice

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const path = require('path');
 
@@ -6,4 +7,16 @@ const publicPath=path.join(__dirname,'public')
 
 app.use(express.static(publicPath));
 
-app.listen(5000);
+app.listen(5000);*/
+
+const express = require("express");
+const path  = require("path");
+
+const app = express();
+const publicPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+app.listen(4848);
+
+// app.use(express.static(publicPath));
