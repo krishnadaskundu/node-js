8a5de85770fdad830b3c28248d866982b6232565
krishnadaskundu <krishnadas.kundu.unique@gmail.com>
2023-05-30 18:52:55 +0530

    practice

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {MongoClient} = require('mongodb')
+/*const {MongoClient} = require('mongodb')
 const url= 'mongodb://localhost:27017';
 const databaseName='e-comm'
 const client= new MongoClient(url);
@@ -13,5 +13,38 @@ async function getData()
 
 
 }
+
+getData();*/
+
+/*const {MongoClient} = require('mongodb');
+const url = 'mongodb://localhost:27017' ;
+const database = 'hotel';
+const client = new MongoClient(url);
+
+async function getData(){
+    let result = await client.connect();
+    db = result.db(database);
+    collection = db.collection('room')
+    let data = await collection.find({}).toArray();
+    console.log(data);
+}
+getData();*/
+
+const {MongoClient} = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const databaseName = 'hotel';
+const client = new MongoClient(url);
+
+
+async function getData() 
+{
+    let result = await client.connect();
+    db = result.db(databaseName);
+    collection = db.collection('room');
+    let data = await collection.find({}).toArray();
+    console.log(data);
+
+}
+
 
 getData();
