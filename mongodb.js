def70eed315a0135687ab1b38681bd5ffc0f2afc
krishnadaskundu <krishnadas.kundu.unique@gmail.com>
2023-06-01 18:08:19 +0530

    practice

--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,13 @@
 const {MongoClient} = require('mongodb')
 const url= 'mongodb://localhost:27017';
-const databaseName='e-comm'
+const databaseName='hotel'
 const client= new MongoClient(url);
 
 async function dbConnect()
 {
     let result = await client.connect();
     db= result.db(databaseName);
-    return db.collection('products');
+    return db.collection('room');
   
 }
 module.exports= dbConnect;
